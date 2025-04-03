022f2ef7ae77f8900061682c9aa06300c0d087ff
tridxis <tridxis.eth@gmail.com>
2025-04-03 23:43:19 +0700

    remove debug

--- variables/target/debug/deps/variables-78b6df3f2866972a.d
+++
@@ -1,5 +0,0 @@
-/Users/andoan/Dev/PLAYGROUND/RUST/get-rusty/variables/target/debug/deps/variables-78b6df3f2866972a: src/main.rs
-
-/Users/andoan/Dev/PLAYGROUND/RUST/get-rusty/variables/target/debug/deps/variables-78b6df3f2866972a.d: src/main.rs
-
-src/main.rs:
--- variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-/Users/andoan/Dev/PLAYGROUND/RUST/get-rusty/variables/target/debug/variables: /Users/andoan/Dev/PLAYGROUND/RUST/get-rusty/variables/src/main.rs
